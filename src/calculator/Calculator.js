67923250cc3a84ac3c22287d337918f7f8fc0f63
katiullahaxioned <atiullahk@axioned>
2022-11-08 18:22:22 +0530

    error handling code implemented & need to solve 3 more errors

--- src/calculator/Calculator.js
+++ src/calculator/Calculator.js
@@ -1,11 +1,9 @@
-import React, { useReducer, useRef } from 'react';
+import React, { useReducer } from 'react';
 import Button from './Button';
 import reducer from './reducer';
 
 const Calculator = () => {
   const [stateValue, dispatch] = useReducer(reducer, '');
-  const inputOne =  useRef();
-  const inputTwo =  useRef();
 
   const operators = ["/","*","+","-","C"];
   const numbers = ["1","2","3","4","5","6","7","8","9",".","0","="];
@@ -30,8 +28,8 @@ const Calculator = () => {
           <div className="calc">
             <form action="#FIXME">
               <div className="input-box">
-                <input type="text" name="inputOne" ref={inputOne} value={stateValue} placeholder="0" readOnly />
-                <input type="text" name="inputTwo" ref={inputTwo} value='' readOnly />
+                <input type="text" value={stateValue} placeholder="0" readOnly />
+                <input type="text" value={stateValue} readOnly />
               </div>
               <div className="operator-box">
                 {
@@ -56,156 +54,3 @@ const Calculator = () => {
 }
 
 export default Calculator;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useReducer, useRef, useState } from 'react';
-// import Button from './Button';
-// import reducer from './reducer';
-
-// const Calculator = () => {
-//   // const [stateValue, setStateValue] = useState('');
-//   const [stateValue, dispatch] = useReducer(reducer, '');
-//   const inputOne =  useRef();
-//   const inputTwo =  useRef();
-
-//   const operators = ["/","*","+","-","C"];
-//   const numbers = ["1","2","3","4","5","6","7","8","9",".","0","="];
-
-//   const addValue = (e) => {
-//     if(e.target.value === "=" ) {
-//       dispatch({type: "EVAL"});
-//       return;
-//     }
-//     dispatch({type: "COMMON", payload: e.target.value});
-//   }
-
-//   // const addValue = (e) => {
-//   //   if(e.target.value === "=" ) {
-//   //     setStateValue((prev) => eval(prev));
-//   //     return;
-//   //   }
-//   //   setStateValue((prev) => (prev + e.target.value));
-//   // }
-
-//   return (
-//     <>
-//       <section className="calculator">
-//         <div className="wrapper">
-//           <h2 className="section-title">react calculator with react hooks</h2>
-//           <div className="calc">
-//             <form action="#FIXME">
-//               <div className="input-box">
-//                 <input type="text" name="inputOne" ref={inputOne} value={stateValue} placeholder="0" readOnly />
-//                 <input type="text" name="inputTwo" ref={inputTwo} value='' readOnly />
-//               </div>
-//               <div className="operator-box">
-//                 {
-//                   operators.map((operator, idx) => {
-//                     return <Button key={idx} type="button" value={operator} addValue={addValue} />
-//                   })
-//                 }
-//               </div>
-//               <div className="number-box">
-//                 {
-//                   numbers.map((number, idx) => {
-//                     return <Button key={idx} type="button" value={number} addValue={addValue} />
-//                   })
-//                 }
-//               </div>
-//             </form>
-//           </div>
-//         </div>
-//       </section>
-//     </>
-//   )
-// }
-
-// export default Calculator;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React, { useRef } from 'react';
-
-// const Calculator = () => {
-
-//   const inputOne =  useRef();
-//   const inputTwo =  useRef();
-
-//   return (
-//     <>
-//       <section className="calculator">
-//         <div className="wrapper">
-//           <h2 className="section-title">react calculator with react hooks</h2>
-//           <div className="calc">
-//             <form action="#FIXME">
-//               <div className="input-box">
-//                 <input type="text" name="inputOne" ref={inputOne} value='' placeholder="0" readOnly />
-//                 <input type="text" name="inputTwo" ref={inputTwo} value='0' readOnly />
-//               </div>
-//               <div className="operator-box">
-//                 <input type="button" value="/" />
-//                 <input type="button" value="*" />
-//                 <input type="button" value="+" />
-//                 <input type="button" value="-" />
-//                 <input type="button" value="C" />
-//               </div>
-//               <div className="number-box">
-//                 <input type="button" value="1" />
-//                 <input type="button" value="2" />
-//                 <input type="button" value="3" />
-//                 <input type="button" value="4" />
-//                 <input type="button" value="5" />
-//                 <input type="button" value="6" />
-//                 <input type="button" value="7" />
-//                 <input type="button" value="8" />
-//                 <input type="button" value="9" />
-//                 <input type="button" value="." />
-//                 <input type="button" value="0" />
-//                 <input type="button" value="=" />
-//               </div>
-//             </form>
-//           </div>
-//         </div>
-//       </section>
-//     </>
-//   )
-// }
-
-// export default Calculator;
--- src/calculator/reducer.js
+++ src/calculator/reducer.js
@@ -1,8 +1,21 @@
 const reducer = (state, {type, payload}) => {
 
+  const regEx = /[+-/.*]/;
+
   switch(type) {
+
     case "COMMON" : {
-        return state + payload;
+      const current = state + payload;
+      const regexMatch1 = regEx.test(current[current.length - 1]);
+      const regexMatch2 = regEx.test(current[current.length - 2]);
+
+      if (regexMatch1 && regexMatch2) {
+        return current.slice(0, current.length - 2) + payload;
+      } else if((current[0] === '0') && (current[1] !== '.')) {
+        return state;
+      } else {
+        return current;
+      }
     }
 
     case "CLEAR" : {
@@ -10,14 +23,22 @@ const reducer = (state, {type, payload}) => {
       return str.slice(0,str.length-1);
     }
     
-    case "EVAL" : {      
-      const output = eval(state);
-      const str = output.toString();
-      if (!str.includes('.')) {
-        return output.toFixed(0);
-      } else {
-        return output.toFixed(3);
-      }
+    case "EVAL" : {
+      if(state) {
+        if(regEx.test(state[state.length-1]) || state[0] === '/' || state[0] === '*') {
+          console.log("invalid expression : ", Math.random().toFixed(3));
+          return state = '';
+        } else {
+          const output = eval(state);
+          const str = output.toString();
+          if (!str.includes('.')) {
+            return parseInt(output.toFixed(0));
+          } else {
+            return parseFloat(output.toFixed(6));
+          }
+        }
+      } 
+      break;
     }
 
     default : console.log("calc error");;
